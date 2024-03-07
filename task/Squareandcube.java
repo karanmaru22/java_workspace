afac6d4203437d255c5eda39220b4d73dda077db
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-03-07 16:57:31 +0530

    Add files via upload

---
+++ task/Squareandcube.java
@@ -0,0 +1,12 @@
+package java_workspace;
+
+public class Squareandcube {
+	public static void main(String[] args)
+	{
+		int a=4;
+		System.out.println((a*a));
+		System.out.println((a*a*a));
+		
+	}
+
+}
