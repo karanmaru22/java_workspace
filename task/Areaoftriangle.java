5946476308e3e1a8c16450608ccc3bfc2b03131f
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-03-07 17:06:44 +0530

    Add files via upload

---
+++ task/Areaoftriangle.java
@@ -0,0 +1,12 @@
+package java_workspace;
+
+public class Areaoftriangle {
+	public static void main(String[] args) 
+	{
+		int base=5;
+		int length=10;
+		
+		System.out.println(0.5*base*length);
+	}
+
+}
