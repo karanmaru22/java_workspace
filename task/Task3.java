52fc52fb2e1664a8a44f91420410e6a7d579fdba
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-05-29 17:29:38 +0530

    Add files via upload

---
+++ task/Task3.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Task3
+{
+	public static void main(String[] args)
+	{
+		
+		int i=100;
+		
+		do 
+		{
+			System.out.println(i);
+			i--;
+		}
+		while(i>=81);
+		
+	}
+
+}
