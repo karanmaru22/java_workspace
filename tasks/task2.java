bc8c8abfdec166d11fa3c33ac4ccf3ff62f9bd0d
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-06-10 17:25:11 +0530

    Add files via upload

---
+++ tasks/task2.java
@@ -0,0 +1,24 @@
+package java_workspace_task;
+
+public class task2
+{
+	public static void main(String[] args) 
+	{
+		
+	int rows=4;
+	int number=1;
+	
+	for(int i=1;i<=rows;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(number + " ");
+			number++;
+		}
+			System.out.println();
+	}
+	}
+	
+}
+	
+
